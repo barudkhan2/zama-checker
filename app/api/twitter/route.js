7c6ca6c6a72bfdca9b3f724133f84a8e999302f9
barudkhan2 <shifatbinjisan@gmail.com>
2025-11-24 18:13:24 +0600

    Update route.js

--- app/api/twitter/route.js
+++ app/api/twitter/route.js
@@ -3,11 +3,14 @@ export async function GET(req) {
   const { searchParams } = new URL(req.url);
   const query = searchParams.get("q") || "crypto";
 
-  const r = await fetch(`https://api.twitter.com/2/tweets/search/recent?query=${query}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const r = await fetch(
+    `https://api.twitter.com/2/tweets/search/recent?query=${query}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 
   const data = await r.json();
   return new Response(JSON.stringify(data), { status: 200 });
